41cc887a2e43cf068f575aaa3a34bb0a8989e74a
joseph <rollins.joseph@gmail.com>
2015-04-01 14:11:58 -0700

    factor out common directories

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,25 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass');
 
+var src_dir = 'src/',
+    sass_dir = 'src/scss/',
+    dest_dir = 'theme/',
+    css_dir = dest_dir + 'css/';
+
 gulp.task('default', ['watch']);
 
 gulp.task('sass', function() {
-  gulp.src('./*.scss')
+  gulp.src(sass_dir + '**/*.scss')
       .pipe(sass())
-      .pipe(gulp.dest('./css'));
+      .pipe(gulp.dest(css_dir));
 });
 
-gulp.task('copy-php', function() {
-  gulp.src('src/**/*.php')
-      .pipe(gulp.dest('theme'));
+gulp.task('php', function() {
+  gulp.src(src_dir + '**/*.php')
+      .pipe(gulp.dest(dest_dir));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./*.scss', ['sass']);
-  gulp.watch('src/**/*.php', ['copy-php']);
+  gulp.watch(sass_dir + '**/*.scss', ['sass']);
+  gulp.watch(src_dir + '**/*.php', ['php']);
 });
