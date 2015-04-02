7ebe8f42bf9408c07493746d36933ca61e00f043
joseph <rollins.joseph@gmail.com>
2015-04-02 08:13:14 -0700

    add wp_footer & copyright notice

--- src/footer.php
+++ src/footer.php
@@ -8,7 +8,11 @@
  */
 ?>
     </div>
+
     <footer>
+      <p>Copyright &copy; <?php echo date('Y'); ?>. All Rights Reserved.</p>
     </footer>
+    
+    <?php wp_footer(); ?>
   </body>
 </html>
