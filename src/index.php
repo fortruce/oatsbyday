46ed5c7d95ad5668e38e1533a9ca5f3ff3d10f95
joseph <rollins.joseph@gmail.com>
2015-04-02 09:20:06 -0700

    make title a permalink to post

--- src/index.php
+++ src/index.php
@@ -13,7 +13,9 @@
         
         <article class="post">
             <header>
-                <h1 class="post__title"><?php the_title(); ?></h1>
+                <h1 class="post__title">
+                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                </h1>
                 <time datetime="<?php the_time('Y-m-d');  ?>"><?php the_time('F j, Y'); ?></time>
             </header>
             <p class="post__content"><?php the_content(); ?></p>
