bdcf2163ba33f365a6bfea91334896fbdd332f30
joseph <rollins.joseph@gmail.com>
2015-04-02 09:10:01 -0700

    add basic wp loop

--- src/index.php
+++ src/index.php
@@ -7,7 +7,24 @@
 ?>
 <?php get_header(); ?>
 
-
- <p>Hello World</p>
+<section class="posts">
+    
+    <?php if (have_posts()): while(have_posts()): the_post(); ?>
+        
+        <article class="post">
+            <header>
+                <h1 class="post__title"><?php the_title(); ?></h1>
+                <time datetime="<?php the_time('Y-m-d');  ?>"><?php the_time('F j, Y'); ?></time>
+            </header>
+            <p class="post__content"><?php the_content(); ?></p>
+        </article>
+        
+    <?php endwhile; else: ?>
+    
+        <p>Sorry, no posts were found.</p>
+    
+    <?php endif; ?>
+    
+</section>
 
 <?php get_footer(); ?>
