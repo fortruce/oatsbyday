6f4621190be142f7411155702a057d284b0633e8
joseph <rollins.joseph@gmail.com>
2015-04-09 14:11:18 -0700

    normalize class names and add basic collage/list styling for homepage

--- src/index.php
+++ src/index.php
@@ -7,36 +7,34 @@
 ?>
 <?php get_header(); ?>
 
-<?php $collage_classes = array('3-of-5', '2-of-5',
-                               '3', '3', '3',
-                               '2', '2',
-                               '4', '4', '4', '4');?>
+<?php $col_classes = array('3-of-5', '2-of-5 col--uneven-small',
+                           '3', '3', '3',
+                           '2', '2',
+                           '4', '4', '4', '4');?>
 
-<div class="posts">
+<div id="layout" class="layout layout--collage">
     
     <?php
-
     $query = new WP_Query(array('post_type' => 'post',
                                 'posts_per_page' => 11));
 
     if ($query->have_posts()):
-              $i = -1;
-              while($query->have_posts()):
-                   $query->the_post();
-                   $i++; ?>
+             $i = -1;
+    while($query->have_posts()):
+                  $query->the_post();
+    $i++; ?>
         
-        <article class="<?php echo 'post post--'. $collage_classes[$i]; ?>">
+        <div class="<?php echo 'post col col--'. $col_classes[$i]; ?>">
             <h1 class="post__title"><?php the_title(); ?></h1>
-                <a class="post__link" href="<?php the_permalink(); ?>">
-                    <?php
-                    echo the_post_thumbnail('post-thumbnail',
-                                            array('class' => 'post__image')); ?>
-                </a>
-        </article>
+            <a href="<?php the_permalink(); ?>">
+                <?php echo the_post_thumbnail('post-thumbnail',
+                                        array('class' => 'post__image')); ?>
+            </a>
+        </div>
         
     <?php endwhile; else: ?>
     
-        <p>Sorry, no posts were found.</p>
+    <p>Sorry, no posts were found.</p>
     
     <?php endif; ?>
     
